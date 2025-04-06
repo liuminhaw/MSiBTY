7598e27fc01d111b14256c288190add9e50af93e
Min-Haw, Liu <liuminhaw@gmail.com>
2025-04-06 22:37:59 +0800

    feat: rust cargo-modules installation

--- runs/rust.sh
+++ runs/rust.sh
@@ -5,11 +5,16 @@ source ${_SCRIPT_DIR}/libs/helper.sh
 # echo "Dry: ${_DRY}"
 # echo "Is Dry: ${_IS_DRY}"
 
+# TODO: Check if rust is already installed, if so run `rustup update`
 if [[ ${_IS_DRY} -eq 1 ]]; then
     echo "[DRY RUN]: curl --proto '=https' --tlsv1.2 https://sh.rustup.rs -sSf | sh"
 else
     echo "[EXECUTE]: curl --proto '=https' --tlsv1.2 https://sh.rustup.rs -sSf | sh"
     curl --proto '=https' --tlsv1.2 https://sh.rustup.rs -sSf | sh
 fi
+
+execute cargo install cargo-modules
+
+
 
 
