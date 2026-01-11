8e2967d4a917ae6064d192a21dcf3bb159afd973
Min-Haw, Liu <liuminhaw@gmail.com>
2026-01-11 11:57:50 +0800

    feat: add new programs
    - codex
    - crush
    - nano

--- runs/tmux.sh
+++ runs/tmux.sh
@@ -4,4 +4,4 @@ source ${_SCRIPT_DIR}/libs/helper.sh
 
 execute sudo pacman -Sy --needed tmux
 
-execute install_aur "${HOME}/builds" "https://aur.archlinux.org/tmux-bash-completion-git.git"
+#execute install_aur "${HOME}/builds" "https://aur.archlinux.org/tmux-bash-completion-git.git"
