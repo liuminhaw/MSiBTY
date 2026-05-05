2445cd8111206594d8c070f32a2d0132645b2f01
Min-Haw <liuminhaw@gmail.com>
2026-05-05 19:58:41 +0800

    feat: update terraform install method

--- runs/terraform.sh
+++ runs/terraform.sh
@@ -5,4 +5,7 @@ source ${_SCRIPT_DIR}/libs/helper.sh
 # echo "Dry: ${_DRY}"
 # echo "Is Dry: ${_IS_DRY}"
 
-execute sudo pacman -Sy --needed terraform
+# execute sudo pacman -Sy --needed terraform
+
+execute install_aur "${HOME}/builds" "https://aur.archlinux.org/tfenv.git"
+execute sudo usermod -a -G tfenv ${USER}
