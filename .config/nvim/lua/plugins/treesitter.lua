c4601e90ef5a48577ea7c8126c71a1b5446a7579
Min-Haw, Liu <liuminhaw@gmail.com>
2026-05-21 21:20:07 +0800

    feat: update settings

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,44 +1,93 @@
-return {
-	"nvim-treesitter/nvim-treesitter",
-	build = ":TSUpdate",
-	branch = "main",
-	lazy = false,
-	config = function()
-		-- local configs = require("nvim-treesitter.configs")
+-- return {
+-- 	"nvim-treesitter/nvim-treesitter",
+-- 	build = ":TSUpdate",
+-- 	branch = "main",
+-- 	lazy = false,
+-- 	config = function()
+-- 		-- local configs = require("nvim-treesitter.configs")
+--
+-- 		-- configs.setup({
+-- 		require('nvim-treesitter').setup({
+-- 			ensure_installed = {
+-- 				"c",
+-- 				"lua",
+-- 				"vim",
+-- 				"vimdoc",
+-- 				"query",
+-- 				"typescript",
+-- 				"javascript",
+-- 				"html",
+-- 				"css",
+-- 				"go",
+-- 				"python",
+-- 				"bash",
+-- 				"json",
+-- 				"yaml",
+-- 				"terraform",
+-- 			},
+-- 			sync_install = false,
+-- 			auto_install = true,
+-- 			highlight = {
+-- 				enable = true,
+-- 				additional_vim_regex_highlighting = false,
+-- 				disable = function(lang, buf)
+-- 					local max_filesize = 100 * 1024 -- 100 KB
+-- 					local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+-- 					if ok and stats and stats.size > max_filesize then
+-- 						return true
+-- 					end
+-- 				end,
+-- 			},
+-- 			indent = { enable = true },
+-- 		})
+-- 	end,
+-- }
 
-		-- configs.setup({
-		require('nvim-treesitter').setup({
-			ensure_installed = {
-				"c",
-				"lua",
-				"vim",
-				"vimdoc",
-				"query",
-				"typescript",
-				"javascript",
-				"html",
-				"css",
-				"go",
-				"python",
-				"bash",
-				"json",
-				"yaml",
-				"terraform",
-			},
-			sync_install = false,
-			auto_install = true,
-			highlight = {
-				enable = true,
-				additional_vim_regex_highlighting = false,
-				disable = function(lang, buf)
-					local max_filesize = 100 * 1024 -- 100 KB
-					local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-					if ok and stats and stats.size > max_filesize then
-						return true
-					end
-				end,
-			},
-			indent = { enable = true },
-		})
-	end,
+return {
+  "nvim-treesitter/nvim-treesitter",
+  branch = "main",
+  build = ":TSUpdate",
+  lazy = false,
+  config = function()
+    require("nvim-treesitter").setup()
+
+    require("nvim-treesitter").install({
+      "c",
+      "lua",
+      "vim",
+      "vimdoc",
+      "query",
+      "typescript",
+      "javascript",
+      "html",
+      "css",
+      "go",
+      "python",
+      "bash",
+      "json",
+      "yaml",
+      "terraform",
+    })
+
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = {
+        "c",
+        "lua",
+        "vim",
+        "typescript",
+        "javascript",
+        "html",
+        "css",
+        "go",
+        "python",
+        "sh",
+        "json",
+        "yaml",
+        "terraform",
+      },
+      callback = function()
+        vim.treesitter.start()
+      end,
+    })
+  end,
 }
--- runs/ruby.sh
+++ runs/ruby.sh
@@ -5,5 +5,5 @@ source ${_SCRIPT_DIR}/libs/helper.sh
 # echo "Dry: ${_DRY}"
 # echo "Is Dry: ${_IS_DRY}"
 
-execute sudo pacman -Sy --needed aws-cli-v2
-execute sudo pacman -Sy --needed python-cfn-lint
+execute sudo pacman -Sy --needed ruby
+
